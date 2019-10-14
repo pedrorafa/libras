593543b8f6cb92d634c5ed24961a7ce6b9b6d206
pedrorafa <pedrorafag00@gmail.com>
2019-10-14 20:34:16 -0300

    update heroku

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/libras-web'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/libras-web/index.html'));
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/libras-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
+console.log('App running on port: ' + process.env.PORT)
