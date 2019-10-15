9a47fc4de652680c78033acf92ed6639e444d89f
pedrorafa <pedrorafag00@gmail.com>
2019-10-14 21:36:50 -0300

    code clean

--- app.js
+++ app.js
@@ -14,4 +14,4 @@ app.get('/*', function (req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
-console.log('App running on port: ' + process.env.PORT)
+console.log('App running on port: ' + (process.env.PORT || 4200))
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ComponentsComponent } from './components/components.component';
@@ -12,24 +12,24 @@ import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.compo
 import { StaticAlphabetComponent } from './pages/games/staticAlphabet/staticAlphabet.component';
 import { HangmanComponent } from './pages/games/hangman/hangman.component';
 
-const routes: Routes =[
-    { path: '', redirectTo: 'staticAlphabet', pathMatch: 'full' },
-    { path: 'home',             component: ComponentsComponent },
-    { path: 'user-profile',     component: ProfileComponent },
-    { path: 'signup',           component: SignupComponent },
-    { path: 'landing',          component: LandingComponent },
-    { path: 'nucleoicons',      component: NucleoiconsComponent },
+const routes: Routes = [
+  { path: '', redirectTo: 'staticAlphabet', pathMatch: 'full' },
+  { path: 'home', component: ComponentsComponent },
+  { path: 'user-profile', component: ProfileComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'nucleoicons', component: NucleoiconsComponent },
 
-    
-    { path: 'hangman',      component: HangmanComponent },
-    { path: 'staticAlphabet',      component: StaticAlphabetComponent }
+
+  { path: 'hangman', component: HangmanComponent },
+  { path: 'staticAlphabet', component: StaticAlphabetComponent }
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
+    RouterModule.forRoot(routes, {
       useHash: true
     })
   ],
