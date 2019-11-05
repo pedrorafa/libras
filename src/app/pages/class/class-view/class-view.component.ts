7525ef583922f02b9b28fe6036bbbd1fe16fbed6
pedrorafa <pedrorafag00@gmail.com>
2019-11-05 19:14:05 -0300

    class-box

--- src/app/pages/class/class-view/class-view.component.ts
+++ src/app/pages/class/class-view/class-view.component.ts
@@ -11,11 +11,24 @@ export class ClassViewComponent implements OnInit {
 
   ngOnInit() {
     this.classes.push({
+      _id: "dsafsasdgaswdfgdasd",
       idGames:[],
+      Title:"Jogo da forca",
+      Description: "Jogo da forca em libras para memorização do alfabeto e interação entre os alunos",
       Level: 1,
       Duration: 10,
       createAt: new Date()
     })
   }
   public classes = []
+  private apiHost = 'https://api-libras.herokuapp.com/api/'
+  
+  editClass(){
+    console.log("Editar "+this.classes[0]._id)
+    fetch(this.apiHost + 'games').then(res => console.log(res))
+
+  }
+  addClass(){
+    console.log("Adicionar nova classe")
+  }
 }
